f18de35aa8b76f61b6f234f97d79fa06a08e0dd1
8t3 <behindthedankmem@gmail.com>
2017-04-20 12:32:33 -0700

    meme init created

---
+++ meme.cpp
@@ -0,0 +1,20 @@
+// created by tai lol
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "stdafx.h"
+
+AllocConsole()
+FreeConsole()
+
+printf("printf test");
+
+cout << "cout test" << cout
+
+printf("meme init succesful");
+printf("launching memehax");
+
+end 
+
+  end
