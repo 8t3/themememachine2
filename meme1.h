fad64d1bbbd9db3f26e7d7321d58c01b01fa5313
8t3 <behindthedankmem@gmail.com>
2017-04-20 12:43:03 -0700

    created meme header file

---
+++ meme1.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "stdafx.h"
+
+printf("Welcome to mememachine2");
+printf("searching for whitelist...");
+
+void hwidWL
+
+int hwidWL
+
+voidProc("WLACCEPT")
+
+printf("Whitelisted!");
+printf("welcome to mememachine2!");
